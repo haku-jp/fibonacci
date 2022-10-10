184b3a851d6a9fd147197403c1907bfbabf09cd2
haku <kounotori1920@gmail.com>
2022-10-10 16:23:38 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict';
+const memo = new Map(); //一度計算した結果を保存することを「メモ化」という
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (memo.has(n)) {
+    return memo.get(n);
   }
   //前と、前の前のフィボナッチ数を足し合わせた数
-  return fib(n - 1) + fib(n - 2);
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
 const length = 100;
 for (let i = 0; i <= length; i++) {
